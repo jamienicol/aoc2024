a4fbbe256628a6d238d06b3339cabf8184ad89cc
Jamie Nicol <jnicol@mozilla.com>
2024-12-11 11:21:58 +0000

    Day 11

--- src/util.rs
+++ src/util.rs
@@ -1,5 +1,9 @@
 use nom::{character::complete::digit1, combinator::map_res, IResult};
 
+pub fn parse_u64(input: &str) -> IResult<&str, u64> {
+    map_res(digit1, |s: &str| s.parse::<u64>())(input)
+}
+
 pub fn parse_usize(input: &str) -> IResult<&str, usize> {
     map_res(digit1, |s: &str| s.parse::<usize>())(input)
 }
